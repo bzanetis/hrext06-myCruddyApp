b4f7a1fdf8c8f21073536ddd7d3de201814919d8
bzanetis <britt.zanetis19@gmail.com>
2018-12-17 16:52:41 -0600

    Changed input to form with two values - displaying/saving/deleting as expected

--- app.js
+++ app.js
@@ -3,18 +3,41 @@ $(document).ready(function(){
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+
+    var title = $('.text-entry').val();
+    var author = $('.text-author').val(); 
+    var bookInfo = title + ",  " + author;
+    //bookInfo.push(title, author);
+    //localStorage.bookInfo += JSON.stringify({ "bookTitle": title, "bookAuthor": author});
+
+    localStorage.setItem('formInputValues', bookInfo);
+    var myItemInStorage = localStorage.getItem('formInputValues');
+    // console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
     $('.list-display-field').text(myItemInStorage); // ??
 
   });
 
+  // need to be able to add multiple vals to display field
+  // try appending myItemInStorage to display field
+  // could try setting myIS to an array --> have a func that iterates & pushes 
+
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    localStorage.removeItem('formInputValues');
   });
 
 });
+
+$("#btn").click(function () {
+
+        var principal = $("#amount").val();
+        var interest = $("#interest").val();
+        var years = $("#period").val();
+        var item = [];
+       item.push(principal,interest,years);
+       localStorage.item += JSON.stringify({ "principalAmount": principal, "interestAmount": interest, "Period": years });
+
+
+    });
